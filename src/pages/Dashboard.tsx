dbadaa288e3137600c6c45b12d9e2a0f8ded9732
shashank <shashank200418@gmail.com>
2024-09-26 22:24:27 +0530

    dashboard fixed

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,11 @@
-
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-semibold text-gray-800 mb-4">Welcome</h1>
+        <p className="text-gray-600">Happy log in or sign up !</p>
+      </div>
+    </div>
   )
 }
 
